487e41e328653f74a33d1d3448c717082bd3ed4d
mithilmittal <mithilmittal@Mithil>
2021-08-10 12:32:56 +0530

    Conflict

--- Greenfield Training Assignment 5/src/pk1/base.java
+++ Greenfield Training Assignment 5/src/pk1/base.java
@@ -3,7 +3,6 @@ package pk1;
 public class base {
 	public static void main(String[] args) {
 		System.out.println("This is added in Branch B");
-		System.out.println("Creating conflict");
-		System.out.println("Conflict created");
+		System.out.println("Conflict Created");
 	}
 }
