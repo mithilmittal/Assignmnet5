1f55c41864370c9cba5d5436f596cabd3e267ac0
mithilmittal <mithilmittal@Mithil>
2021-08-10 11:33:46 +0530

    Made changes in print statement.

--- Greenfield Training Assignment 5/src/pk1/base.java
+++ Greenfield Training Assignment 5/src/pk1/base.java
@@ -2,7 +2,7 @@ package pk1;
 
 public class base {
 	public static void main(String[] args) {
-		System.out.println("This is an example");
+		System.out.println("This is an example. Changes made");
 	}
 
 }
