71e3f22decdb33b64aa76751091b8124906e7f16
mithilmittal <mithilmittal@Mithil>
2021-08-10 12:01:34 +0530

    creating conflict

--- Greenfield Training Assignment 5/src/pk1/base.java
+++ Greenfield Training Assignment 5/src/pk1/base.java
@@ -4,8 +4,6 @@ public class base {
 	public static void main(String[] args) {
 		System.out.println("This is added in Branch B");
 		System.out.println("Creating conflict");
-		creating an error
-		doing nothing
-	}
-
+		wrong syntax
+		}
 }
