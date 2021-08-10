952d1b59543d3854e9e560ddc7b7e3c63f490e37
mithilmittal <mithilmittal@Mithil>
2021-08-10 11:58:20 +0530

    creating conflict

--- Greenfield Training Assignment 5/src/pk1/base.java
+++ Greenfield Training Assignment 5/src/pk1/base.java
@@ -3,8 +3,9 @@ package pk1;
 public class base {
 	public static void main(String[] args) {
 		System.out.println("This is added in Branch B");
-		System.out.println("TRYING SOMETHING NEW");
-		This line will not work. This is wrong.
+		System.out.println("Creating conflict");
+		creating an error
+		doing nothing
 	}
 
 }
